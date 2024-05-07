c925d945e2e9a907b177722b0322536bec8b4a0c
claire891018 <110798482+claire891018@users.noreply.github.com>
2024-05-08 04:03:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,78 @@
 import streamlit as st
-import app_component as ac 
+import app.component as ac 
 
-import utils as u
+import streamlit as st
+import tempfile
+from langchain.document_loaders import PyMuPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_google_genai import GoogleGenerativeAI
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
 
-def input_fields():
-    st.session_state.source_docs = st.file_uploader(label="上傳文件！", type="pdf", accept_multiple_files=True)
+@st.cache_data
 
-def process_documents():
-    if not google_api_key:
-        st.warning("請上傳你的 api key！")
-    else:
-        try:
-            for source_doc in st.session_state.source_docs:
-                texts = u.load_and_chunk(source_doc)
-                st.session_state.retriever = u.create_embeddings_and_vectordb(texts)
-            if "retriever" in st.session_state:
-                st.success("資料處理完成，歡迎提問！")
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
+def db_retriever(uploaded_file):
+    # Load document if file is uploaded
+    if uploaded_file is not None:
+        documents = [uploaded_file.read().decode()]
+        # Split documents into chunks
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.create_documents(documents)
+        # Select embeddings
+        model_name = "aspire/acge_text_embedding"
+        model_kwargs = {'device': 'cpu'}
+        embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
+        # Create a vectorstore from documents
+        db = Chroma.from_documents(texts, embeddings)
+        # Create retriever interface
+        retriever = db.as_retriever()
+    
+        return retriever
+    
+def boot():    
+    # File upload
+    uploaded_file = st.file_uploader('Upload an article', type='txt')
 
-def boot():
-    st.button("上傳文件", on_click=process_documents())
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-    for message in st.session_state.messages:
-        st.chat_message('human').write(message[0])
-        st.chat_message('ai').write(message[1])
-    if query := st.chat_input("在此輸入問題..."):
-        st.chat_message("human").write(query)
-        if "retriever" in st.session_state:
-            response = u.query_llm(st.session_state.retriever, query)
-        else:
-            response = u.query_llm_direct(query)
-        st.chat_message("ai").write(response)
+    # Query text
+    if "messages" not in st.session_state or st.sidebar.button("清除歷史資料"):
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-#with st.spinner("思考中..."):
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    if query := st.chat_input(placeholder="詢問跟資料相關的問題！"):
+        st.session_state.messages.append({"role": "user", "content": query})
+        st.chat_message("user").write(query)
+
+        if not google_api_key:
+            st.warning("請上傳你的 api key！")
+            st.stop()
+
+        llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=google_api_key)
+
+        prompt = """
+        資料：{context}
+        你能協助閱讀資料，並且總是能夠立即、準確地回答任何要求。
+        請用繁體中文(traditonal chinese)回答下列問題。不知道就回答「資訊未提及：{query}」，並確保答案內容相關且簡潔。
+        問題：{query}
+        回答：
+        """
+
+        qa_Google = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff", # can be adjusted
+            retriever=db_retriever(uploaded_file),
+            return_source_documents=True,
+            chain_type_kwargs={"prompt": prompt},
+            #verbose=True 控制輸出詳細程度
+        )
+
+        with st.chat_message("assistant"):
+            response = qa_Google.invoke({"query": query})
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            st.write(response)
 
 if __name__ == '__main__':
     st.set_page_config(
@@ -48,8 +86,6 @@ if __name__ == '__main__':
     mode = st.sidebar.radio("LLM type：", ('上傳你的 Google API Key',))
     if mode == '上傳你的 Google API Key':
         google_api_key = st.sidebar.text_input('Google API Key:', type='password')
-    input_fields()
-    #file_pdf = st.file_uploader(label="上傳文件！", type="pdf", accept_multiple_files=True)
     boot()
 
 
