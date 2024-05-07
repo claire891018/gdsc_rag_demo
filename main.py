b443e4493de6c064a794a7826be0d5c2743121be
claire891018 <110798482+claire891018@users.noreply.github.com>
2024-05-07 22:43:46 +0000

    modified:   main.py 	modified:   requirements.txt 	deleted:    utils.py

--- main.py
+++ main.py
@@ -12,86 +12,81 @@ from langchain_community.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAI
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 
-@st.cache(allow_output_mutation=True)
+@st.cache_resource(ttl="1h")
+def configure_retriever(uploaded_files):
+    # read documents
+    docs = []
+    temp_dir = tempfile.TemporaryDirectory()
+    for file in uploaded_files:
+        temp_filepath = os.path.join(temp_dir.name, file.name)
+        with open(temp_filepath, "wb") as f:
+            f.write(file.getvalue())
+        loader = PyPDFLoader(temp_filepath)
+        docs.extend(loader.load())
 
-def generator(uploaded_file, google_api_key, query):
-    # Load document if file is uploaded
-    if uploaded_file is not None:
-        try:
-            temp_dir = tempfile.mkdtemp()
-            path = os.path.join(temp_dir, uploaded_file.name)
-            with open(path, "wb") as f:
-                f.write(uploaded_file.getvalue())
-            #temp_file = "./temp.pdf"
-            #with open(temp_file, "wb") as file:
-            #   file.write(uploaded_file.getvalue())
-            #   file_name = uploaded_file.name
-            loader = PyPDFLoader(path)
-            #loader = PyMuPDFLoader(uploaded_file.name)
-            documents = loader.load_and_split()
-        
-            # Split documents into chunks
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=10)
-            all_splits = text_splitter.split_documents(documents)
-            
-            # Select embeddings
-            model_name = "aspire/acge_text_embedding"
-            model_kwargs = {'device': 'cpu'}
-            embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
-            # Create a vectorstore from documents
-            db = Chroma.from_documents(all_splits, embeddings, persist_directory="db")
-            # Create retriever interface
-            retriever = db.as_retriever()
-            
-            llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=google_api_key)
-            template = """
-            資料：{context}
-            你能協助閱讀資料，並且總是能夠立即、準確地回答任何要求。
-            請用繁體中文(traditonal chinese)回答下列問題。不知道就回答「資訊未提及：{query}」，並確保答案內容相關且簡潔。
-            問題：{query}
-            回答：
-            """
-            prompt = PromptTemplate.from_template(template=template)
-            qa_Google = RetrievalQA.from_chain_type(
-                llm=llm,
-                chain_type="stuff", # can be adjusted
-                retriever=retriever,
-                return_source_documents=True,
-                chain_type_kwargs={"prompt": prompt},
-                #verbose=True 控制輸出詳細程度
-            )
-            result = qa_Google.invoke({"query": query})
-            response = result["result"]
-            return response
+    # split documents
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    splits = text_splitter.split_documents(docs)
 
-        except Exception as e:
-            st.error(f"資料解析失敗，原因：{e}")
-            return None
+    # create embeddings and store in vectordb
+    embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-large-en-v1.5")
+    vectordb = Chroma.from_documents(splits, embeddings)
+
+    # define retriever
+    retriever = vectordb.as_retriever(
+        search_type="mmr", search_kwargs={"k": 4, "fetch_k": 10}
+    )
+    return retriever  
     
 def boot(): 
     # File upload
-    uploaded_file = st.file_uploader('請上傳資料', type='pdf')
+    uploaded_files = st.sidebar.file_uploader(
+        label="Upload PDF files", type=["pdf"], accept_multiple_files=True
+    )
+    if not uploaded_files:
+        st.info("Please upload PDF documents to continue.")
+        st.stop()
 
-    # Query text
-    if "messages" not in st.session_state or st.sidebar.button("清除歷史資料"):
-        st.session_state["messages"] = [{"role": "assistant", "content": "請根據資料提問"}]
+    retriever = configure_retriever(uploaded_files)
+    msgs = StreamlitChatMessageHistory()
 
-    for msg in st.session_state.messages:
-        st.chat_message(msg["role"]).write(msg["content"])
+    llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=google_api_key)
+    template = """
+        資料：{context}
+        你能協助閱讀資料，並且總是能夠立即、準確地回答任何要求。
+        請用繁體中文(traditonal chinese)回答下列問題。不知道就回答「資訊未提及：{query}」，並確保答案內容相關且簡潔。
+        問題：{query}
+        回答：
+    """
+    prompt = PromptTemplate.from_template(template=template, input_variables=["context", "query"])
+    qa_Google = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff", # can be adjusted
+        retriever=retriever,
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": prompt},
+        #verbose=True 控制輸出詳細程度
+    )
 
-    if query := st.chat_input(placeholder="詢問跟資料相關的問題！"):
-        st.session_state.messages.append({"role": "user", "content": query})
+    if len(msgs.messages) == 0 or st.sidebar.button("新對話"):
+        msgs.clear()
+        msgs.add_ai_message("需要什麼協助～")
+
+    avatars = {"human": "user", "ai": "assistant"}
+    for msg in msgs.messages:
+        st.chat_message(avatars[msg.type]).write(msg.content)
+
+    if query := st.chat_input(placeholder="Ask me anything!"):
         st.chat_message("user").write(query)
 
-        if not google_api_key:
-            st.warning("請上傳你的 api key！")
-            st.stop()
-
         with st.chat_message("assistant"):
-            response = generator(uploaded_file, google_api_key, query)
-            st.session_state.messages.append({"role": "assistant", "content": response})
-            st.write(response)
+            response = qa_Google({"query": query})
+
+            ## print answer
+            answer = response["result"]
+            st.write(answer)
 
 if __name__ == '__main__':
     st.set_page_config(
